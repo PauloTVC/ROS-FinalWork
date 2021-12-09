dc5ec6440d7e026ad20b95dcb950e66eb8e8660f
PauloTVC <83144900+PauloTVC@users.noreply.github.com>
2021-12-08 21:09:44 -0300

    Update controle_SOLO_ROS.py

--- ros_final/scripts/controle_SOLO_ROS.py
+++ ros_final/scripts/controle_SOLO_ROS.py
@@ -7,7 +7,8 @@ from math import  sqrt
 from tf2_geometry_msgs import PointStamped
 from geometry_msgs.msg import Pose2D, Twist
 from tf2_ros import buffer
-from ros_final.msg import poseStp
+from ros_final.msg import PointStampedArray
+
 
 #Input Speed of Cente of Mass by 
 #It will be feed by the Main 
@@ -18,10 +19,8 @@ def Spd(data):
      return speed
 
 #Create the PointStampeds
-def vect(xp, yp, zp, time):
+def vect(xp, yp, zp):
      po = PointStamped()
-     po.header.frame_id = "base_link"
-     po.header.stamp = time
      po.point.x = xp
      po.point.y = yp
      po.point.z = zp
@@ -34,7 +33,7 @@ def main():
      buffer = tf2_ros.Buffer()
      listener = tf2_ros.TransformListener(buffer)
      rospy.Subscriber("speed", Twist, Spd)
-     P = rospy.Publisher("points", poseStp, queue_size=10)
+     P = rospy.Publisher("points", PointStampedArray, queue_size=10)
      r = rospy.Rate(10)
 
      #speed
@@ -49,17 +48,19 @@ def main():
      pe = s/(4*spd)
 
      #Array of PoinStamped
-     pv = poseStp 
+     pv = PointStampedArray
+     pv.header.frame_id="base_link"
+     pv.header.stamp = pe 
 
 
      #hidden leg right
-     pv[0] = vect(s -0.08*sqrt(2), w/2, h, pe)
+     pv.points[0] = vect(s -0.08*sqrt(2), w/2, h)
      #hidden leg left
-     pv[1] = vect(s -0.08*sqrt(2), -w/2, h, pe)
+     pv.points[1] = vect(s -0.08*sqrt(2), -w/2, h)
      #forward leg right
-     pv[2] = vect(s +0.08*sqrt(2), w/2, h, pe)
+     pv.points[2] = vect(s +0.08*sqrt(2), w/2, h)
      #forward leg left
-     pv[3] = vect(s +0.08*sqrt(2), -w/2, h, pe)
+     pv.points[3] = vect(s +0.08*sqrt(2), -w/2, h)
 
      #publishing
      P.publish(pv)
@@ -71,4 +72,3 @@ if __name__ == '__main__':
         main()
     except rospy.ROSInterruptException:
         pass
-
